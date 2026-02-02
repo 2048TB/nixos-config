65ea1ccd95f13df5cfccf40aae1a4febe126c568
Your Name <your.email@example.com>
2026-02-03 00:02:34 +0800

    fix: update fcitx5 chinese addons package name

--- modules/desktop-input.nix
+++ modules/desktop-input.nix
@@ -8,7 +8,7 @@
     fcitx5.addons = with pkgs; [
       qt6Packages.fcitx5-configtool
       fcitx5-gtk
-      fcitx5-chinese-addons  # 中文拼音输入法
+      qt6Packages.fcitx5-chinese-addons  # 中文拼音输入法
     ];
   };
 }
