f6f8cf256cb1df84175c09da5d68e04c960581ed
2048TB <2048TB@users.noreply.github.com>
2026-03-11 22:02:08 +0800

    refactor: deduplicate nixos host eval checks

--- nix/hosts/nixos/zky/checks.nix
+++
@@ -1,8 +0,0 @@
-args:
-let
-  hostVars = import ./vars.nix;
-in
-import ../_shared/checks.nix (args // {
-  expectedLuksName = hostVars.luksName or "crypted-nixos";
-  expectedResumeOffset = hostVars.resumeOffset or null;
-})
