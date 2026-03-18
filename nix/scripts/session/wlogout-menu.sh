f48c5811c3f371fb3d9a7d668c72673d8ed537eb
2048TB <165404998+2048TB@users.noreply.github.com>
2026-03-18 09:16:31 +0800

    feat: refine waybar and wlogout styling

--- nix/scripts/session/wlogout-menu.sh
+++ nix/scripts/session/wlogout-menu.sh
@@ -5,6 +5,8 @@ exec wlogout \
   --buttons-per-row 6 \
   --column-spacing 12 \
   --row-spacing 12 \
+  --margin-top 330 \
+  --margin-bottom 330 \
   -l "$HOME/.config/wlogout/layout" \
   -C "$HOME/.config/wlogout/style.css" \
   "$@"
