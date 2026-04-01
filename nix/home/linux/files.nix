a512f5426373ebc0637b66fc80cd0c49cb2ae775
2048TB <165404998+2048TB@users.noreply.github.com>
2026-04-01 23:37:32 +0800

    fix: restore mingw cgo linker paths

--- nix/home/linux/files.nix
+++ nix/home/linux/files.nix
@@ -12,10 +12,33 @@ in
       recursive = true;
     };
 
+    "tools/x86_64-w64-mingw32-gcc" = {
+      executable = true;
+      text = ''
+        #!${pkgs.runtimeShell}
+        exec ${pkgs.pkgsCross.mingwW64.stdenv.cc}/bin/x86_64-w64-mingw32-gcc \
+          -L${pkgs.pkgsCross.mingwW64.windows.mcfgthreads}/lib \
+          -L${pkgs.pkgsCross.mingwW64.windows.pthreads}/lib \
+          "$@"
+      '';
+    };
+
+    "tools/x86_64-w64-mingw32-g++" = {
+      executable = true;
+      text = ''
+        #!${pkgs.runtimeShell}
+        exec ${pkgs.pkgsCross.mingwW64.stdenv.cc}/bin/x86_64-w64-mingw32-g++ \
+          -L${pkgs.pkgsCross.mingwW64.windows.mcfgthreads}/lib \
+          -L${pkgs.pkgsCross.mingwW64.windows.pthreads}/lib \
+          "$@"
+      '';
+    };
+
     ".cargo/config.toml".text = ''
       [target.x86_64-pc-windows-gnu]
-      linker = "x86_64-w64-mingw32-gcc"
+      linker = "${homeDir}/tools/x86_64-w64-mingw32-gcc"
       rustflags = [
+        "-Lnative=${pkgs.pkgsCross.mingwW64.windows.mcfgthreads}/lib",
         "-Lnative=${pkgs.pkgsCross.mingwW64.windows.pthreads}/lib"
       ]
     '';
